eb7336adc99e465bda692a9ff2ad96784c80499c
Ronak Pasricha <37462015+rpasricha45@users.noreply.github.com>
2024-11-11 10:33:27 -0500

    test azure sample part 2

--- src/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-pytest.register_assert_rewrite('src.tests.test_functions')
-
-def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "asyncio: mark test as async"
-    ) 
